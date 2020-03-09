6e6a2de9798f56918b0d70b5fff1fd2af3bc4283
MarcusBA96 <mbadum-abe@hotmail.com>
2020-03-09 13:49:49 +0100

    parallel clean

---
+++ parallel_clean.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+from multiprocessing import Pool
+
+def parallelize_dataframe(df, func, n_cores=4):
+    df_split = np.array_split(df, n_cores)
+    pool = Pool(n_cores)
+    df = pd.concat(pool.map(func, df_split))
+    pool.close()
+    pool.join()
+    return df
+

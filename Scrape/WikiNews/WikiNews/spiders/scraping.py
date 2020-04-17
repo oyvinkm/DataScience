91f0e756efa94a3d7c39114015ac2783e0033fa3
Øyvin Moxness <oyvin.moxvoll@gmail.com>
2020-04-17 13:36:52 +0200

    Scraper updated with links

--- Scrape/WikiNews/WikiNews/spiders/scraping.py
+++ Scrape/WikiNews/WikiNews/spiders/scraping.py
@@ -7,7 +7,13 @@ class WikiSpider(scrapy.Spider):
     allowed_domains = ['wikinews.org']
     chars = "ABCDEFGHIJKLMNOPRSTUVWZABCDEFGHIJKLMNOPRSTUVWZ"[35%23:35%23+10]
     start_urls = ['https://en.wikinews.org/w/index.php?title=Category:Politics_and_conflicts&from='+char for char in chars]
+    base_url = 'https://en.wikinews.org'
+    links = []
     def parse(self,response):
+        links = response.xpath('/html/body/div[3]/div[3]/div[4]/div[2]/div[1]/div/div/div[1]/ul//a/@href').extract()
+        for link in links:
+            newlink = str(self.base_url + link)
         pass
 
 
+urlsFromCategory = response.xpath('/html/body/div[3]/div[3]/div[4]/div[2]/div[2]/div/div//a/@href').extract()

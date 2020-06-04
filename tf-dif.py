91717f993a246592bb6de950904c1d33084164b4
MarcusBA96 <mbadum-abe@hotmail.com>
2020-06-04 15:50:42 +0200

    machine learning is fun

--- tf-dif.py
+++ tf-dif.py
@@ -18,12 +18,12 @@ X = vectorizer.fit_transform(rawData["content"]).toarray()
 Y = list(rawData["type"])
 clf_svm = svm.SVC(kernel='linear', class_weight='balanced', gamma='auto', probability=True)
 clf_svm.fit(X,Y)
-#clf_kNeighbors = KNeighborsClassifier(n_neighbors = 5)
-#clf_kNeighbors.fit(X,Y)
+clf_kNeighbors = KNeighborsClassifier(n_neighbors = 5)
+clf_kNeighbors.fit(X,Y)
 clf_naiveBayes = GaussianNB()
 clf_naiveBayes.fit(X,Y)
-ensemble=VotingClassifier(estimators=[('SVM', clf_svm), ('naiveBayese', clf_naiveBayes)], 
-                       voting='soft', weights=[1,2]).fit(X,Y)
+ensemble=VotingClassifier(estimators=[('SVM', clf_svm), ('naiveBayes', clf_naiveBayes), ('KNN', clf_kNeighbors)], 
+                       voting='soft', weights=[1,1,1]).fit(X,Y)
 
 testData = execquery.execQuery("""SELECT contents, articleID FROM articles Where articleID Between 1000 and 2000;""")
 testDataRaw = pd.DataFrame(testData, columns=["content", "articleID"])
@@ -33,7 +33,7 @@ vectorizer1 = sk.TfidfVectorizer(max_features=3000)
 X1 = vectorizer1.fit_transform(testDataRaw["content"]).toarray()
 Y1 = list(testRawTypes["type"])
 Y2 = clf_svm.predict(X1)
-#Y3 = clf_kNeighbors.predict(X1)
+Y3 = clf_kNeighbors.predict(X1)
 Y4 = clf_naiveBayes.predict(X1)
 Y5 = ensemble.predict(X1)
 
@@ -48,8 +48,8 @@ correct_counter_ensemble = 0
 for i in range(len(Y1)):
     if (Y1[i] == Y2[i]):
         correct_counter_svm += 1
-    #if (Y1[i] == Y3[i]):
-     #   correct_counter_kNeigh += 1
+    if (Y1[i] == Y3[i]):
+        correct_counter_kNeigh += 1
     if (Y1[i] == Y4[i]):
         correct_counter_naiveBayes += 1
     if (Y4[i] == Y5[i]):
@@ -58,3 +58,22 @@ print(correct_counter_svm)
 print(correct_counter_kNeigh)
 print(correct_counter_naiveBayes)
 print(correct_counter_ensemble)
+
+vectorizer2 = sk.TfidfVectorizer(max_features=3000)
+testDataRaw2 = pd.read_csv("test_data.csv")
+X5 = vectorizer2.fit_transform(testDataRaw2["article"].values.astype('U')).toarray()
+Y6 = ensemble.predict(X5)
+for i, y in enumerate(Y6):
+    if y == "reliable":
+        Y6[i] = "REAL"
+    if y == "NULL":
+        Y6[i] = "FAKE"
+    if y == "fake":
+        Y6[i] = "FAKE"
+        
+testDataRaw2["label"] = Y6
+testDataRaw2.drop('article', axis=1)
+testDataRaw2.astype({'id': 'int32'})
+testDataRaw2.to_csv("predictions.csv", index=False)
+
+
